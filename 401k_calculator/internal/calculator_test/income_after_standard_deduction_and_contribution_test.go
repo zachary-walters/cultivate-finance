17cfe982d5b5bd803a3dae4c6becf19fa152c665
zachary-walters <zcw.dev@gmail.com>
2024-06-06 17:25:25 -0500

    Added tests for income after standard deduction and contributions

--- 401k_calculator/internal/calculator_test/income_after_standard_deduction_and_contribution_test.go
+++ 401k_calculator/internal/calculator_test/income_after_standard_deduction_and_contribution_test.go
@@ -33,67 +33,130 @@ func (m *MockIncomeAfterStandardDeductionAndContributions) CalculateRothRetireme
 	return args.Get(0).(float64)
 }
 
-func TestIncomeAfterstandardDeductionAndContributionsCalculate(t *testing.T) {
-	tests := []struct {
-		name                         string
-		model                        calculator.Model
-		incomeAfterStandardDeduction float64
-	}{
-		{
-			name: "Test Case Basic",
-			model: calculator.Model{
-				Input: calculator.Input{
-					AnnualContributionsPreTax: 1337,
-				},
+var incomeAfterStandardDeductionAndContributionTests = []struct {
+	name                         string
+	model                        calculator.Model
+	incomeAfterStandardDeduction float64
+}{
+	{
+		name: "Test Case Basic",
+		model: calculator.Model{
+			Input: calculator.Input{
+				AnnualContributionsPreTax: 1337,
 			},
-			incomeAfterStandardDeduction: 42,
 		},
-		{
-			name: "Test Case 0",
-			model: calculator.Model{
-				Input: calculator.Input{
-					AnnualContributionsPreTax: 0,
-				},
+		incomeAfterStandardDeduction: 42,
+	},
+	{
+		name: "Test Case 0",
+		model: calculator.Model{
+			Input: calculator.Input{
+				AnnualContributionsPreTax: 0,
 			},
-			incomeAfterStandardDeduction: 42,
 		},
-		{
-			name:  "Test Case Empty",
-			model: calculator.Model{},
-		},
-		{
-			name: "Test Case Infinity",
-			model: calculator.Model{
-				Input: calculator.Input{
-					AnnualContributionsPreTax: math.MaxFloat64,
-				},
+		incomeAfterStandardDeduction: 42,
+	},
+	{
+		name:  "Test Case Empty",
+		model: calculator.Model{},
+	},
+	{
+		name: "Test Case Infinity",
+		model: calculator.Model{
+			Input: calculator.Input{
+				AnnualContributionsPreTax: math.MaxFloat64,
 			},
-			incomeAfterStandardDeduction: 42,
 		},
-		{
-			name: "Test Case Negative Infinity",
-			model: calculator.Model{
-				Input: calculator.Input{
-					AnnualContributionsPreTax: -math.MaxFloat64,
-				},
+		incomeAfterStandardDeduction: 42,
+	},
+	{
+		name: "Test Case Negative Infinity",
+		model: calculator.Model{
+			Input: calculator.Input{
+				AnnualContributionsPreTax: -math.MaxFloat64,
 			},
-			incomeAfterStandardDeduction: 42,
 		},
+		incomeAfterStandardDeduction: 42,
+	},
+}
+
+func TestNewIncomeAfterStandardDeductionAndContributions(t *testing.T) {
+	actual := calculator.NewIncomeAfterStandardDeductionAndContributions()
+	expected := calculator.IncomeAfterStandardDeductionAndContributions{
+		IncomeAfterStandardDeductionCalculation: calculator.NewIncomeAfterStandardDeduction(),
 	}
 
-	mockIncomeAfterStandardDeduction := new(MockIncomeAfterStandardDeduction)
+	assert.Equal(t, expected, actual)
+}
 
-	c := &calculator.IncomeAfterStandardDeductionAndContributions{
-		IncomeAfterStandardDeductionCalculation: mockIncomeAfterStandardDeduction,
-	}
-
-	for _, test := range tests {
+func TestIncomeAfterStandardDeductionAndContributionsCalculateTraditional(t *testing.T) {
+	for _, test := range incomeAfterStandardDeductionAndContributionTests {
+		mockIncomeAfterStandardDeduction := new(MockIncomeAfterStandardDeduction)
 		mockIncomeAfterStandardDeduction.On("CalculateTraditional", mock.Anything).Return(test.incomeAfterStandardDeduction)
+
+		c := &calculator.IncomeAfterStandardDeductionAndContributions{
+			IncomeAfterStandardDeductionCalculation: mockIncomeAfterStandardDeduction,
+		}
+
 		t.Run(test.name, func(t *testing.T) {
 			actual := c.CalculateTraditional(&test.model)
 			expected := func() float64 {
-				return float64(c.IncomeAfterStandardDeductionCalculation.CalculateTraditional(&test.model)) - test.model.Input.AnnualContributionsPreTax
+				return test.incomeAfterStandardDeduction - test.model.Input.AnnualContributionsPreTax
 			}()
+			assert.Equal(t, expected, actual)
+		})
+	}
+}
+
+func TestIncomeAfterStandardDeductionAndContributionsCalculateTraditionalRetirement(t *testing.T) {
+	for _, test := range incomeAfterStandardDeductionAndContributionTests {
+		t.Run(test.name, func(t *testing.T) {
+			mockIncomeAfterStandardDeduction := new(MockIncomeAfterStandardDeduction)
+			mockIncomeAfterStandardDeduction.On("CalculateTraditional", mock.Anything).Return(test.incomeAfterStandardDeduction)
+
+			c := &calculator.IncomeAfterStandardDeductionAndContributions{
+				IncomeAfterStandardDeductionCalculation: mockIncomeAfterStandardDeduction,
+			}
+
+			actual := c.CalculateTraditionalRetirement(&test.model)
+			expected := c.CalculateTraditional(&test.model)
+
+			assert.Equal(t, expected, actual)
+		})
+	}
+}
+
+func TestIncomeAfterStandardDeductionAndContributionsCalculateRoth(t *testing.T) {
+	for _, test := range incomeAfterStandardDeductionAndContributionTests {
+		t.Run(test.name, func(t *testing.T) {
+			mockIncomeAfterStandardDeduction := new(MockIncomeAfterStandardDeduction)
+			mockIncomeAfterStandardDeduction.On("CalculateTraditional", mock.Anything).Return(test.incomeAfterStandardDeduction)
+
+			c := &calculator.IncomeAfterStandardDeductionAndContributions{
+				IncomeAfterStandardDeductionCalculation: mockIncomeAfterStandardDeduction,
+			}
+
+			actual := c.CalculateRoth(&test.model)
+			expected := c.CalculateTraditional(&test.model)
+
+			assert.Equal(t, expected, actual)
+		})
+	}
+}
+
+func TestIncomeAfterStandardDeductionAndContributionsCalculateRothRetirement(t *testing.T) {
+	for _, test := range incomeAfterStandardDeductionAndContributionTests {
+		t.Run(test.name, func(t *testing.T) {
+			mockIncomeAfterStandardDeduction := new(MockIncomeAfterStandardDeduction)
+			mockIncomeAfterStandardDeduction.On("CalculateTraditional", mock.Anything).Return(test.incomeAfterStandardDeduction)
+
+			c := &calculator.IncomeAfterStandardDeductionAndContributions{
+				IncomeAfterStandardDeductionCalculation: mockIncomeAfterStandardDeduction,
+			}
+
+			actual := c.CalculateRothRetirement(&test.model)
+			expected := c.CalculateTraditional(&test.model)
+
 			assert.Equal(t, expected, actual)
 		})
 	}
