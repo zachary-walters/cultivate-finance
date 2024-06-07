252986672cae5e2fdae0121f7c3e908d588e8383
zachary-walters <zcw.dev@gmail.com>
2024-06-07 13:51:20 -0500

    Added unit tests for net distribution after taxes

--- 401k_calculator/internal/calculator_test/net_distribution_after_taxes_test.go
+++ 401k_calculator/internal/calculator_test/net_distribution_after_taxes_test.go
@@ -32,30 +32,40 @@ func (m *MockNetDistributionAfterTaxes) CalculateRothRetirement(model *calculato
 	return args.Get(0).(float64)
 }
 
-func TestNetDistributionAfterTaxesCalculateTraditional(t *testing.T) {
-	tests := []struct {
-		name                                 string
-		totalTaxesOwedAfterStandadDeduction  float64
-		totalAnnualRetirementIncomeBeforeTax float64
-	}{
-		{
-			name:                                 "Test Case 0",
-			totalTaxesOwedAfterStandadDeduction:  1337,
-			totalAnnualRetirementIncomeBeforeTax: 3942039,
-		},
-		{
-			name:                                 "Test Case 1",
-			totalTaxesOwedAfterStandadDeduction:  1337,
-			totalAnnualRetirementIncomeBeforeTax: 3447,
-		},
-		{
-			name: "Test Case 2",
-		},
+var netDistributionAfterTaxesTests = []struct {
+	name                                 string
+	totalTaxesOwedAfterStandadDeduction  float64
+	totalAnnualRetirementIncomeBeforeTax float64
+}{
+	{
+		name:                                 "Test Case 0",
+		totalTaxesOwedAfterStandadDeduction:  1337,
+		totalAnnualRetirementIncomeBeforeTax: 3942039,
+	},
+	{
+		name:                                 "Test Case 1",
+		totalTaxesOwedAfterStandadDeduction:  1337,
+		totalAnnualRetirementIncomeBeforeTax: 3447,
+	},
+	{
+		name: "Test Case 2",
+	},
+}
+
+func TestNewNetDistributionAfterTaxes(t *testing.T) {
+	actual := calculator.NewNetDistributionAfterTaxes()
+	expected := calculator.NetDistributionAfterTaxes{
+		TotalTaxesOwedAfterStandardDeductionCalculation: calculator.NewTotalTaxesOwedAfterStandardDeduction(),
+		TotalAnnualRetirementIncomeBeforeTaxCalculation: calculator.NewTotalAnnualRetirementIncomeBeforeTax(),
 	}
 
+	assert.Equal(t, expected, actual)
+}
+
+func TestNetDistributionAfterTaxesCalculateTraditional(t *testing.T) {
 	testModel := calculator.Model{}
 
-	for _, test := range tests {
+	for _, test := range netDistributionAfterTaxesTests {
 		t.Run(test.name, func(t *testing.T) {
 			mockTotalTaxesOwedAfterStandardDeduction := new(MockTotalTaxesOwedAfterStandardDeduction)
 			mockTotalTaxesOwedAfterStandardDeduction.On("CalculateTraditional", &testModel).Return(test.totalTaxesOwedAfterStandadDeduction)
@@ -69,6 +79,78 @@ func TestNetDistributionAfterTaxesCalculateTraditional(t *testing.T) {
 			}
 
 			actual := c.CalculateTraditional(&testModel)
+			expected := test.totalAnnualRetirementIncomeBeforeTax - test.totalTaxesOwedAfterStandadDeduction
+
+			assert.Equal(t, expected, actual)
+		})
+	}
+}
+
+func TestNetDistributionAfterTaxesCalculateTraditionalRetirement(t *testing.T) {
+	testModel := calculator.Model{}
+
+	for _, test := range netDistributionAfterTaxesTests {
+		t.Run(test.name, func(t *testing.T) {
+			mockTotalTaxesOwedAfterStandardDeduction := new(MockTotalTaxesOwedAfterStandardDeduction)
+			mockTotalTaxesOwedAfterStandardDeduction.On("CalculateTraditionalRetirement", &testModel).Return(test.totalTaxesOwedAfterStandadDeduction)
+
+			mockTotalAnnualRetirementIncomeBeforeTaxCalculation := new(MockTotalAnnualRetirementIncomeBeforeTax)
+			mockTotalAnnualRetirementIncomeBeforeTaxCalculation.On("CalculateTraditionalRetirement", &testModel).Return(test.totalAnnualRetirementIncomeBeforeTax)
+
+			c := calculator.NetDistributionAfterTaxes{
+				TotalTaxesOwedAfterStandardDeductionCalculation: mockTotalTaxesOwedAfterStandardDeduction,
+				TotalAnnualRetirementIncomeBeforeTaxCalculation: mockTotalAnnualRetirementIncomeBeforeTaxCalculation,
+			}
+
+			actual := c.CalculateTraditionalRetirement(&testModel)
+			expected := test.totalAnnualRetirementIncomeBeforeTax - test.totalTaxesOwedAfterStandadDeduction
+
+			assert.Equal(t, expected, actual)
+		})
+	}
+}
+
+func TestNetDistributionAfterTaxesCalculateRoth(t *testing.T) {
+	testModel := calculator.Model{}
+
+	for _, test := range netDistributionAfterTaxesTests {
+		t.Run(test.name, func(t *testing.T) {
+			mockTotalTaxesOwedAfterStandardDeduction := new(MockTotalTaxesOwedAfterStandardDeduction)
+			mockTotalTaxesOwedAfterStandardDeduction.On("CalculateRoth", &testModel).Return(test.totalTaxesOwedAfterStandadDeduction)
+
+			mockTotalAnnualRetirementIncomeBeforeTaxCalculation := new(MockTotalAnnualRetirementIncomeBeforeTax)
+			mockTotalAnnualRetirementIncomeBeforeTaxCalculation.On("CalculateRoth", &testModel).Return(test.totalAnnualRetirementIncomeBeforeTax)
+
+			c := calculator.NetDistributionAfterTaxes{
+				TotalTaxesOwedAfterStandardDeductionCalculation: mockTotalTaxesOwedAfterStandardDeduction,
+				TotalAnnualRetirementIncomeBeforeTaxCalculation: mockTotalAnnualRetirementIncomeBeforeTaxCalculation,
+			}
+
+			actual := c.CalculateRoth(&testModel)
+			expected := test.totalAnnualRetirementIncomeBeforeTax - test.totalTaxesOwedAfterStandadDeduction
+
+			assert.Equal(t, expected, actual)
+		})
+	}
+}
+
+func TestNetDistributionAfterTaxesCalculateRothRetirement(t *testing.T) {
+	testModel := calculator.Model{}
+
+	for _, test := range netDistributionAfterTaxesTests {
+		t.Run(test.name, func(t *testing.T) {
+			mockTotalTaxesOwedAfterStandardDeduction := new(MockTotalTaxesOwedAfterStandardDeduction)
+			mockTotalTaxesOwedAfterStandardDeduction.On("CalculateRothRetirement", &testModel).Return(test.totalTaxesOwedAfterStandadDeduction)
+
+			mockTotalAnnualRetirementIncomeBeforeTaxCalculation := new(MockTotalAnnualRetirementIncomeBeforeTax)
+			mockTotalAnnualRetirementIncomeBeforeTaxCalculation.On("CalculateRothRetirement", &testModel).Return(test.totalAnnualRetirementIncomeBeforeTax)
+
+			c := calculator.NetDistributionAfterTaxes{
+				TotalTaxesOwedAfterStandardDeductionCalculation: mockTotalTaxesOwedAfterStandardDeduction,
+				TotalAnnualRetirementIncomeBeforeTaxCalculation: mockTotalAnnualRetirementIncomeBeforeTaxCalculation,
+			}
+
+			actual := c.CalculateRothRetirement(&testModel)
 			expected := test.totalAnnualRetirementIncomeBeforeTax - test.totalTaxesOwedAfterStandadDeduction
 
 			assert.Equal(t, expected, actual)
